491b9adeb13caa519cf22edc25bed81f15123ce5
TairovichJR <yokubov.omonjon@gmail.com>
2021-06-19 01:12:59 -0400

    MissingNumber

--- src/easy/MissingNumber.java
+++ src/easy/MissingNumber.java
@@ -1,31 +1,17 @@
 package easy;
 
+
 import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * Created by tairovich_jr on 2021-06-19.
  */
 public class MissingNumber {
 
-//    public int missingNumber(int[] nums) {
-//
-//        Arrays.sort(nums);
-//        int result = 0;
-//        for (int i = 1; i < nums.length; i++) {
-//            if (nums[i] - nums[i-1] > 1){
-//                result =  nums[i] - 1;
-//                break;
-//            }
-//        }
-//        if (result == 0 && nums[0] != 0){
-//            return result;
-//        }
-//        if(result == 0 && nums[0] == 0){
-//            return nums[nums.length-1] + 1;
-//        }
-//        return result ;
-//    }
+    public int missingNumber2(int[] nums) {
+        return (nums.length * (nums.length + 1))/2 - Arrays.stream(nums).sum();
+    }
+
 
     public int missingNumber(int[] nums) {
         int n = nums.length;
@@ -36,6 +22,6 @@ public class MissingNumber {
         return sum;
     }
     public static void main(String[] args) {
-        System.out.println(new MissingNumber().missingNumber(new int[]{3,4,5,7}));
+        System.out.println(new MissingNumber().missingNumber2(new int[]{0,1,2,4}));
     }
 }
