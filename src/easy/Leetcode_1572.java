c085f885cb5d68be64b08983d077ca0aea3e9a2c
TairovichJR <yokubov.omonjon@gmail.com>
2021-09-01 00:05:40 -0400

    diagonalSum solution

---
+++ src/easy/Leetcode_1572.java
@@ -0,0 +1,29 @@
+package easy;
+
+/**
+ * Created by tairovich_jr on 2021-08-31.
+ */
+public class Leetcode_1572 {
+
+    public static int diagonalSum(int[][] mat) {
+
+        int sum = 0;
+        for (int i = 0, j = 0, k = mat.length-1; i < mat.length; i++, j++, k--) {
+            if (j != k)
+                sum += mat[i][j] + mat[i][k];
+            else
+                sum += mat[i][j];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3,4},
+                {1,2,4,5},
+                {1,2,3,8},
+                {8,6,1,2}
+        };
+        System.out.println(diagonalSum(arr));
+    }
+}
