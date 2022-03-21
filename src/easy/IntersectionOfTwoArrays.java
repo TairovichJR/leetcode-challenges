844a86f8ceac2974265b768cab34ea31af8adb43
TairovichJR <yokubov.omonjon@gmail.com>
2022-03-20 20:43:18 -0400

    IntersectionOfTwoArrays 2

--- src/easy/IntersectionOfTwoArrays.java
+++ src/easy/IntersectionOfTwoArrays.java
@@ -1,6 +1,8 @@
 package easy;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by tairovich_jr on 2022-03-20.
@@ -11,18 +13,29 @@ public class IntersectionOfTwoArrays {
     public int[] intersection(int[] nums1, int[] nums2) {
 
 
-        String nums = "";
+//        String nums = "";
+//        for (int i = 0; i < nums1.length; i++) {
+//            for (int j = 0; j < nums2.length; j++) {
+//                if (nums1[i] == nums2[j]){
+//                    nums += nums1[i] + ",";
+//                }
+//            }
+//        }
+//        if (nums.isEmpty()) {
+//            return new int[]{};
+//        }
+//        return Arrays.stream(nums.split(",")).distinct().mapToInt(Integer::parseInt).toArray();
+
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
                 if (nums1[i] == nums2[j]){
-                    nums += nums1[i] + ",";
+                    set.add(nums1[i]);
                 }
             }
         }
-        if (nums.isEmpty()) {
-            return new int[]{};
-        }
-        return Arrays.stream(nums.split(",")).distinct().mapToInt(Integer::parseInt).toArray();
+
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) {
