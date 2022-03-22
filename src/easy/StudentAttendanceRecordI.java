5f30f2c6f0b98e7cbd6fdc776d5e58315d31f850
TairovichJR <yokubov.omonjon@gmail.com>
2022-03-22 12:49:16 -0400

    StudentAttendanceRecordI

---
+++ src/easy/StudentAttendanceRecordI.java
@@ -0,0 +1,31 @@
+package easy;
+
+/**
+ * Created by tairovich_jr on 2022-03-22.
+ */
+public class StudentAttendanceRecordI {
+
+
+    public boolean checkRecord(String s){
+
+        int absent = 0;
+        int late = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A'){
+                late = 0;
+                if (++absent >= 2) {
+                    return false;
+                }
+            }
+            if (s.charAt(i) == 'L'){
+                if (++late >= 3){
+                    return false;
+                }
+            }else{
+                late = 0;
+            }
+        }
+        return true;
+    }
+
+}
