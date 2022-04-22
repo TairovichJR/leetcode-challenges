2cc6769b1ed057e6e659c1c2b4fa9cd78965d251
TairovichJR <yokubov.omonjon@gmail.com>
2022-04-21 22:54:50 -0400

    TriangularSumOfArray

---
+++ src/easy/TriangularSumOfArray.java
@@ -0,0 +1,18 @@
+package easy;
+
+public class TriangularSumOfArray {
+
+    public static int triangularSum(int[] nums) {
+        for (int i = nums.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                int n = nums[j] + nums[j+1];
+                nums[j] = n%10;
+            }
+        }
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(triangularSum(new int[]{5}));
+    }
+}
