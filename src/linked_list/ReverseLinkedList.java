8a862a224813734de10279ac65909c711d644032
TairovichJR <yokubov.omonjon@gmail.com>
2022-04-28 01:44:19 -0400

    reverseList recursively

--- src/linked_list/ReverseLinkedList.java
+++ src/linked_list/ReverseLinkedList.java
@@ -19,17 +19,29 @@ public class ReverseLinkedList {
     }
 
 
+    public static ListNode reverseListRecursively(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        ListNode tail = reverseListRecursively(head.next);
+        head.next.next = head;
+        head.next = null;
+        return tail;
+    }
+
+
     public static void main(String[] args) {
 
-//        ListNode head = new ListNode(1);
-//        ListNode node1 = new ListNode(2);
-//        ListNode node2 = new ListNode(3);
-//        ListNode node3 = new ListNode(4);
-//        head.next = node1;
-//        node1.next = node2;
-//        node2.next = node3;
-//
-//        reverseList(head);
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+
+        System.out.println((reverseListRecursively(head).next.next.next.val) );
 
     }
 
